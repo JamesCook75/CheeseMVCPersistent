8027b6d486437cd556ee37db9cbd9f6c2abc2f18
James Cook <jamescook.work@gmail.com>
2018-02-28 12:52:44 -0500

    Part 2 complete, no prepopulated categories

--- src/CheeseMVC/Migrations/20180228173042_AddCategories.Designer.cs
+++ src/CheeseMVC/Migrations/20180228173042_AddCategories.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180228173042_AddCategories")]
+    partial class AddCategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
